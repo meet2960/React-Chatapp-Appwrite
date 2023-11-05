927c9473967fd44622ed7db11a2db8e910aa6bda
ghelanimeet-infiniteai <meet.ghelani@infiniteai.in>
2023-11-05 16:09:11 +0530

    fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,8 +10,8 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    "no-unused-vars": "warn",
-    "@typescript-eslint/no-unused-vars": "warn",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
